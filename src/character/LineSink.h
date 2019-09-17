e393243cb8be5703ffdba0bac23b35be31215686
Łukasz Iwaszkiewicz <lukasz.iwaszkiewicz@gmail.com>
2019-09-17 12:47:15 +0200

    LineSink name refactored, FixedLineSink removed since the new LineSink has its features

--- src/character/LineSink.h
+++ src/character/LineSink.h
@@ -24,86 +24,18 @@
 template <typename QueueT, typename LineT> class LineSink : public ICharacterSink {
 public:
         enum class CanLooseData { NO, YES };
+        enum class LineEnd { CR_LF, CR, LF, EITHER };
 
-        explicit LineSink (QueueT &g, CanLooseData canLooseData = CanLooseData::NO) : receivedDataQueue (g), canLooseData (canLooseData) {}
+        explicit LineSink (QueueT &g, LineEnd lineEnd = LineEnd::CR_LF, CanLooseData canLooseData = CanLooseData::NO)
+            : receivedDataQueue (g), canLooseData (canLooseData), lineEnd (lineEnd)
+        {
+        }
+
         LineSink (LineSink const &) = delete;
         LineSink &operator= (LineSink const &) = delete;
         LineSink (LineSink &&) = delete;
         LineSink &operator= (LineSink &&) = delete;
         ~LineSink () override = default;
-
-        void onData (uint8_t c) override;
-        void onError (uint32_t /* error */) override { Error_Handler (); }
-
-//#define ALL_DATA_DEBUG
-#ifdef ALL_DATA_DEBUG
-        static constexpr size_t ALL_DATA_MAX_SIZE = 2048;
-        uint8_t allData[ALL_DATA_MAX_SIZE];
-        size_t allDataCnt = 0;
-
-        void addAllData (uint8_t c)
-        {
-                if (allDataCnt < ALL_DATA_MAX_SIZE) {
-                        allData[allDataCnt++] = c;
-                }
-        }
-#endif
-
-protected:
-        uint16_t rxBufferGsmPos = 0;
-        LineT line; /// Bufor wejściowy na odpowiedzi z modemu. Mamy własny, gdyż kolejka może się w między czasie wyczyścić.
-        QueueT &receivedDataQueue;
-        CanLooseData canLooseData;
-};
-
-/*****************************************************************************/
-
-template <typename QueueT, typename LineT> void LineSink<QueueT, LineT>::onData (uint8_t c)
-{
-#ifdef ALL_DATA_DEBUG
-        addAllData (c);
-#endif
-
-        if (!bool(std::isprint (c)) && c != '\r' && c != '\n') {
-                return;
-        }
-
-        if (!line.empty () && (c == '\r' || c == '\n')) {
-                gsl::final_action clearPos{ [this] { line.clear (); } };
-
-                // Nie wrzucamy na kolejkę odpowiedzi, które zawierają tylko \r\n
-                if (line.size () > 1) {
-                        if (receivedDataQueue.full ()) {
-                                if (canLooseData == CanLooseData::YES) {
-                                        return;
-                                }
-
-                                Error_Handler ();
-                        }
-
-                        receivedDataQueue.push_back (std::move (line));
-                }
-        }
-        else if (bool(std::isprint (c))) {
-                line.push_back (c);
-        }
-}
-
-template <typename QueueT, typename LineT> class LineSink2 : public ICharacterSink {
-public:
-        enum class CanLooseData { NO, YES };
-        enum class LineEnd { CR_LF, CR, LF, EITHER };
-
-        explicit LineSink2 (QueueT &g, LineEnd lineEnd = LineEnd::CR_LF, CanLooseData canLooseData = CanLooseData::NO)
-            : receivedDataQueue (g), canLooseData (canLooseData), lineEnd (lineEnd)
-        {
-        }
-
-        LineSink2 (LineSink2 const &) = delete;
-        LineSink2 &operator= (LineSink2 const &) = delete;
-        LineSink2 (LineSink2 &&) = delete;
-        LineSink2 &operator= (LineSink2 &&) = delete;
-        ~LineSink2 () override = default;
 
         void onData (uint8_t c) override;
         void onError (uint32_t /* error */) override { Error_Handler (); }
@@ -126,16 +58,16 @@ public:
 
 protected:
         uint16_t rxBufferGsmPos = 0;
-        LineT line; /// Bufor wejściowy na odpowiedzi z modemu. Mamy własny, gdyż kolejka może się w między czasie wyczyścić.
-        QueueT &receivedDataQueue;
+        LineT line;                /// Current line we are assembling.
+        QueueT &receivedDataQueue; /// Queue we will place line onto when finished.
         CanLooseData canLooseData;
         LineEnd lineEnd;
-        size_t fixedNumberOfBytes = 0;
+        size_t fixedNumberOfBytes = 0; /// Alternative mode where we do not split on CRLFs but we split after N bytes has been received.
 };
 
 /*****************************************************************************/
 
-template <typename QueueT, typename LineT> void LineSink2<QueueT, LineT>::onData (uint8_t c)
+template <typename QueueT, typename LineT> void LineSink<QueueT, LineT>::onData (uint8_t c)
 {
 #ifdef ALL_DATA_DEBUG
         addAllData (c);
